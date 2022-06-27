8e6fec510e3521e5575175e9daee096281fcad75
fumikito <guy@hametuha.com>
2022-06-27 23:05:43 +0900

    Update action file.

--- bin/build.sh
+++ bin/build.sh
@@ -2,4 +2,12 @@
 
 set -e
 
-
+rm -rf tests/
+rm -rf doc/
+rm -rf bin/
+rm -rf tests/
+rm -rf .gitattributes
+rm -rf .gitignore
+rm -rf composer.lock
+rm -rf phpdoc.dist.xml
+rm -rf phpunit.xml.dist 
